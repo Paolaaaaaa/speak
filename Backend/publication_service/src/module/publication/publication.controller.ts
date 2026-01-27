ae981613445c42a6417d58fed973fff7dbf62027
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2026-01-27 04:13:19 -0500

    add get and get all

--- Backend/publication_service/src/module/publication/publication.controller.ts
+++ Backend/publication_service/src/module/publication/publication.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PublicationService } from './publication.service';
 
 @Controller('publication')
-export class PublicationController {}
+export class PublicationController {
+  constructor(private publicationService: PublicationService) {}
+  @Get()
+  async findAll() {
+    return await this.publicationService.listPublications();
+  }
+
+  @Get(':publicationID')
+  async findById(@Param('publicationID') publicationID: string) {
+    return await this.publicationService.getPublication(publicationID);
+  }
+}
