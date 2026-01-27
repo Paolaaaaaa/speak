cd16898eac7a8eb0518d0220f2d0745f5da6a9d4
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2026-01-26 20:42:53 -0500

    add folders for the backend Modules add connection to db add typeORM

---
+++ Backend/auth_service/src/users/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { AuthenticatorEntity } from 'src/authenticator/entities/authenticator.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ length: 36 })
+  fullname: string;
+  @Column({ length: 100 })
+  picture: string;
+  @Column({ length: 50 })
+  email: string;
+  @Column({ type: 'date' })
+  last_update: Date;
+  @Column({ type: 'date' })
+  creation_Date: Date;
+  @Column({ length: 100 })
+  profile_description: string;
+
+  @OneToOne(
+    () => AuthenticatorEntity,
+    (authenticator) => authenticator.username,
+    { nullable: true },
+  )
+  @JoinColumn()
+  authenticator: AuthenticatorEntity;
+}
